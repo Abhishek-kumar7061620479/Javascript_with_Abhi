109ad42e39eb4bc1e99f66deb0b23ed9964aeae9
Abhishek-kumar7061620479 <95942551+Abhishek-kumar7061620479@users.noreply.github.com>
2025-08-30 15:55:38 +0000

    commit

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Abhishek")
+console.log("Abhishek")  //use for printing
+
+// constant & variables
+
+const accountId = 123321  // const-> use to declare constant.
+
+let accountName = "salaried"  // let -> use to declare variables.   
+var accountHolder = "Abhishek kumar"  // var -> old method to declare variables.
+
+accountBank = "HDFC"  
+
+
+console.log(accountHolder) // use to print on terminal 
+
+console.table([accountHolder , accountId , accountName , accountBank]) // use to print in tables .
+
+
